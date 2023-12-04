3114be29bfcb675c95287363824918266081ca30
Filip Lorenc <Filip.Lorenc@student.souepl.cz>
2023-12-04 09:33:15 +0100

    zaklad kalkulacky

---
+++ index.py
@@ -0,0 +1,14 @@
+int = x
+int = y
+print("sčítání")
+print(x+y)
+print("odčítání")
+print(x-y)
+print("násobení")
+print(x*y)
+print("dělení")
+print(x/y)
+print("mocnění")
+print(x**y)
+print("odmocnění")
+print(x**(1/y))
